347e8fb16ef778232a68860dc829e538b53fc1e1
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2023-01-01 10:52:23 +0300

    C#
    C#

---
+++ GelecegiYazanlarTurkcellAlgoritma-Sınıflar 4-Çokbiçimlilik/ÇokBiçimlilik/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ÇokBiçimlilik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            papagan p = new papagan();
+            p.sescikar();
+
+            Console.WriteLine("Ses: "+ p.sescikar());
+            Console.WriteLine();
+            Console.WriteLine();
+
+            karga k = new karga();
+            k.agirlik = 1350;
+            k.hiz = 80;
+            k.isim = "Alacakarga";
+            k.renk = "Siyah";
+            k.tur = "Karga";
+            k.sescikar();
+
+            Console.WriteLine("Ağırlık "+ k.agirlik);
+            Console.WriteLine("Hız:  "+k.hiz);
+            Console.WriteLine("İsim: "+k.isim);
+            Console.WriteLine("Renk: "+k.renk);
+            Console.WriteLine("Hız:  "+k.tur);
+            Console.WriteLine("Tür:  "+k.sescikar());
+
+
+
+        }
+    }
+}
---
+++ GelecegiYazanlarTurkcellAlgoritma-Sınıflar 4-Çokbiçimlilik/ÇokBiçimlilik/papagan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÇokBiçimlilik
+{
+    internal class papagan:kuslar
+    {
+        public override string sescikar()
+        {
+            return "cik cik";
+        }
+    }
+}
